cd8b824d45a7744c2cd8dd803223a0386c032b8b
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-01 22:28:43 -0300

    first commit

---
+++ lib/pages/catalogo/catalogo_appbar.dart
@@ -0,0 +1,38 @@
+import 'package:cart_provider_demo/models/carrinho.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CatalogoAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CatalogoAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    print('BUILD: CatalogoAppBar');
+
+    final itemCount = Provider.of<CarrinhoModel>(context);
+    var car = itemCount.items.length;
+
+
+    return AppBar(
+      title: const Text('Catalogo'),
+      actions: [
+        IconButton(
+          icon: Badge(
+            label: Text(
+              '$car',
+              
+              style: TextStyle(color: Colors.white),
+            ),
+            child: const Icon(
+              Icons.shopping_cart,
+            ),
+          ),
+          onPressed: () => Navigator.pushNamed(context, '/carrinho'),
+        )
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60.0);
+}
